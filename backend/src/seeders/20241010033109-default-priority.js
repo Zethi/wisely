0d9e13836ff01aafbfbf45c508fdfe07de33f059
Zethi <diego1900196@gmail.com>
2024-10-13 06:58:17 -0300

    fix: weird error

--- backend/src/seeders/20241010033109-default-priority.js
+++ backend/src/seeders/20241010033109-default-priority.js
@@ -1,21 +1,24 @@
 'use strict';
 
-export async function up(queryInterface, Sequelize) {
-  await queryInterface.bulkInsert('Priority', [
-    {
-      name: 'Baja',
-      weight: 1,
-    },
-    {
-      name: 'Media',
-      weight: 2,
-    },
-    {
-      name: 'Alta',
-      weight: 3,
-    },
-  ], {});
-}
-export async function down(queryInterface, Sequelize) {
-  await queryInterface.bulkDelete('Priority', null, {});
-}
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Priority', [
+      {
+        name: 'Baja',
+        weight: 1,
+      },
+      {
+        name: 'Media',
+        weight: 2,
+      },
+      {
+        name: 'Alta',
+        weight: 3,
+      },
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Priority', null, {});
+  },
+};
