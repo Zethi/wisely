8ab1098c1a53997995dd425e8ecc2d4d7f3de37b
Zethi <diego1900196@gmail.com>
2024-10-13 06:54:03 -0300

    update: env variable name

--- backend/src/config/appConfig.ts
+++ backend/src/config/appConfig.ts
@@ -2,7 +2,7 @@ import { Dialect } from "sequelize";
 import { AppConfigInterface } from "../types/interfaces/appConfig.interface";
 
 export const appConfig: AppConfigInterface = {
-    port: process.env.PORT ?? 3000,
+    port: process.env.APP_PORT ?? 3000,
     db: {
         host: process.env.DB_HOST!,
         port: process.env.DB_PORT!,
